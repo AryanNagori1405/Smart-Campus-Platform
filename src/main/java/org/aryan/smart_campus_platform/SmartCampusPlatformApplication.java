5b9f486913f82be54d4405fd60d87f173c2a5a49
Aryan Nagori <aryannikill123@gmail.com>
2026-09-08 01:01:25 +0530

    initial spring boot project

---
+++ src/main/java/org/aryan/smart_campus_platform/SmartCampusPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.aryan.smart_campus_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampusPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartCampusPlatformApplication.class, args);
+    }
+
+}
