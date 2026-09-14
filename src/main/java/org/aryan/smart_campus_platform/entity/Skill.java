b52143d6eb7faf56504d9214848851e174c6f452
Aryan Nagori <aryannikill123@gmail.com>
2026-09-15 00:59:31 +0530

    Add Skill entity

---
+++ src/main/java/org/aryan/smart_campus_platform/entity/Skill.java
@@ -0,0 +1,32 @@
+package org.aryan.smart_campus_platform.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Skill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank
+    private String name;
+
+    public Skill() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
